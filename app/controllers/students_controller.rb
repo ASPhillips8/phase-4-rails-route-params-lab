e70c18be5e9de33cb443c02aeb308c4bdcfd54da
Anthony Phillips <anthony.s.phillips888@gmail.com>
2024-08-07 15:27:07 -0400

    Name return for index

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,13 +1,21 @@
 class StudentsController < ApplicationController
 
   def index
-    students = Student.all
+    name = params[:name]&.titleize
+
+    students =
+      if name
+        Student.contains_name(name)
+      else
+        Student.all
+      end
+
     render json: students
   end
 
   def show
     student = Student.find(params[:id])
     render json: student
-  end
 
+  end
 end
--- app/models/student.rb
+++ app/models/student.rb
@@ -1,7 +1,7 @@
 class Student < ApplicationRecord
+  scope :contains_name, ->(name) { where("first_name = ? OR last_name = ?", name, name) }
 
   def to_s
     "#{first_name} #{last_name}"
   end
-
 end
